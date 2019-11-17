136d4fced2d789a049439e3f01ead90f6ef7a384
Gatrrr <davidgatrmacaulay@gmail.com>
2019-11-16 23:48:30 -0800

    Added Migrations + Seed, Worked on sever + auth

--- api/server.js
+++ api/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+require('dotenv').config()
 
-server.use(helmet());
-server.use(cors());
+const auth = require('../auth/authRouter.js')
+const customer = require('../customer/customerRouter.js')
+const worker = require('../worker/workerRouter.js')
+
+const server = express();
 server.use(express.json());
+server.use(cors());
+server.use(helmet());
+
+server.use('./api/auth/', auth)
+server.use('./api/customer', customer)
+server.use('./api/worker', worker)
+
+server.get('/', (req,res) => {
+    res.send('Hello!')
+})
 
 module.exports = server;
 
