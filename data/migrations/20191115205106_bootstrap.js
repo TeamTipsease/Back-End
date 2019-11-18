240ff7f222f87e418ff83465b4d0c02c3b4db922
Robert Benavidez <murabitbenavidez@gmail.com>
2019-11-18 10:30:36 -0500

    edited knexfile for production and add pg dependency

--- data/migrations/20191115205106_bootstrap.js
+++ data/migrations/20191115205106_bootstrap.js
@@ -1,56 +1,42 @@
-// exports.up = function(knex) {
-//     return knex.schema
-//     .createTable('auth', table => {
-//         table.increments();
-//         table.string('username', 164)
-//         .notNullable()
-//         .unique();
-//         table.string('role', 255)
-//         .notNullable()
-//         table.string('password', 164)
-//         .notNullable();
-//     })
+exports.up = function(knex) {
+    return knex.schema
+    .createTable('users', table => {
+        table.increments();
+        table.string('username', 164)
+            .notNullable()
+            .unique();
+        table.string('password', 164)
+            .notNullable();
+    })
         
 
-//     .createTable('home', tbl => {
-//         tbl.increments();
-//         tbl.string('name', 255)
-//         .notNullable()
-//         .unique();
-//         tbl.string('description', 500)
-//         .notNullable()
-//         tbl.integer('user_id') //come back to this not sure if foreign key
-//     })
-   
-//     .createTable('service_worker', tbl => {
-//         tbl.increments();
-//         tbl.string('name', 255)
-//         .notNullable()
-//         .unique();
-//         // tbl.image('thumbnail') - Don't quite know how to make an image here
-//         // .notNullable()
-//         // .unique();
-//         tbl.string('info', 500)
-//         .notNullable()
-//         tbl.date('work_length') 
-//         tbl.string('tagline', 500)
-//         .notNullable()
-//         tbl.float('tip')
-//         .notNullable()
-//         tbl.integer('worker_id')
-//         .unsigned()
-//         .notNullable()
-//         .references('id')
-//         .inTable('home')
-//         .onDelete('RESTRICT')
-//         .onUpdate('CASCADE');
-//     })
-// };
+    .createTable('service_worker', tbl => {
+        tbl.increments();
+        tbl.string('name', 255)
+        .notNullable()
+        .unique();
+        // tbl.image('thumbnail') - Don't quite know how to make an image here
+        // .notNullable()
+        // .unique();
+        tbl.string('info', 500)
+        .notNullable()
+        tbl.date('work_length') 
+        tbl.string('tagline', 500)
+        .notNullable()
+        tbl.float('tip')
+        .notNullable()
+        tbl.integer('worker_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('home')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE');
+    })
+};
 
-// exports.down = function(knex) {
-//     return knex.schema
-//     .dropTableIfExists('auth')
-//     .dropTableIfExists('home')
-//     .dropTableIfExists('service_worker')
-  
-// };
+exports.down = function(knex) {
+    return knex.schema
+    .dropTableIfExists('service_worker')
+    .dropTableIfExists('users')
+};
--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,13 @@
-// Update with your config settings.
+require('dotenv').config();
 
 module.exports = {
 
     development: {
-      client: 'sqlite3',
+      client: 'pg',
       connection: {
-        filename: './data/tips.db3',
+        database: process.env.DB_DEV_DATABASE,
+        user: process.env.DB_DEV_USER,
+        password: process.env.DB_DEV_PASSWORD
       },
       useNullAsDefault: true,
       migrations: {
@@ -29,35 +31,21 @@ module.exports = {
       },
     },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
+  
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
     }
   }
 
