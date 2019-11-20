767dee2f6d5e42978911fc0379c4d2b6d8e322ee
Gatrrr <davidgatrmacaulay@gmail.com>
2019-11-20 12:14:49 -0800

    Attempting to work postgres

--- knexfile.js
+++ knexfile.js
@@ -1,87 +1,80 @@
-// require('dotenv').config();
+require('dotenv').config();
 
-// module.exports = {
+module.exports = {
 
-//     development: {
-//       client: 'pg',
-//       connection: {
-//         database: process.env.DB_DEV_DATABASE,
-//         user: process.env.DB_DEV_USER,
-//         password: process.env.DB_DEV_PASSWORD
-//       },
-//       useNullAsDefault: true,
-//       migrations: {
-//         directory: './data/migrations',
-//       },
-//       seeds: {
-//         directory: './data/seeds',
-//       },
-//     },
-//     testing: {
-//       client: 'sqlite3',
-//       connection: {
-//         filename: './data/test1.db3',
-//       },
-//       useNullAsDefault: true,
-//       migrations: {
-//         directory: './data/migrations',
-//       },
-//       seeds: {
-//         directory: './data/seeds',
-//       },
-//     },
+    development: {
+      client: 'sqlite3',
+      connection: {
+        filename: './data/tips.db3'
+      },
+      useNullAsDefault: true,
+      migrations: {
+        directory: './data/migrations',
+      },
+      seeds: {
+        directory: './data/seeds',
+      },
+    },
+    testing: {
+      client: 'sqlite3',
+      connection: {
+        filename: './data/tips.db3',
+      },
+      useNullAsDefault: true,
+      migrations: {
+        directory: './data/migrations',
+      },
+      seeds: {
+        directory: './data/seeds',
+      },
+    },
 
   
 
-//   production: {
-//     client: 'pg',
-//     connection: process.env.DATABASE_URL,
-//     pool: {
-//       min: 2,
-//       max: 10
-//     },
-//     migrations: {
-//       tableName: 'knex_migrations',
-//       directory: './data/migrations'
-//     },
-//     seeds: {
-//       directory: './data/seeds'
-//     }
-//   }
-
-// };
-
-module.exports = { 
-
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './data/tips.db3',
-    },
-    useNullAsDefault: true,
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     migrations: {
-      directory: './data/migrations',
+      directory: './data/migrations'
     },
     seeds: {
-      directory: './data/seeds',
-    },
-  },
-  testing: {
-    client: 'sqlite3',
-    connection: {
-      filename: './data/test.db3',
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: './data/migrations',
-    },
-    seeds: {
-      directory: './data/seeds',
-    },
-  },
-
-staging: {
-  client: 'postgresql',
-}
+      directory: './data/seeds'
+    }
+  }
 
 };
+
+// module.exports = { 
+
+//   development: {
+//     client: 'sqlite3',
+//     connection: {
+//       filename: './data/tips.db3',
+//     },
+//     useNullAsDefault: true,
+//     migrations: {
+//       directory: './data/migrations',
+//     },
+//     seeds: {
+//       directory: './data/seeds',
+//     },
+//   },
+//   testing: {
+//     client: 'sqlite3',
+//     connection: {
+//       filename: './data/test.db3',
+//     },
+//     useNullAsDefault: true,
+//     migrations: {
+//       directory: './data/migrations',
+//     },
+//     seeds: {
+//       directory: './data/seeds',
+//     },
+//   },
+
+// staging: {
+//   client: 'postgresql',
+// }
+
+// };
